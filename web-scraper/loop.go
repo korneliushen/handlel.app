1e1d1954db21551f122a94d3efc159df95278c94
Elias Rissanen Uran <elura001@osloskolen.no>
2024-05-15 09:48:25 +0200

    getting data for each cursor

--- web-scraper/loop.go
+++ web-scraper/loop.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math"
 	"strconv"
 	"strings"
@@ -10,131 +9,36 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// func getCategories() {
-// 	c := colly.NewCollector()
-
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Visiting: ", r.URL)
-// 	})
-
-// 	c.OnError(func(_ *colly.Response, err error) {
-// 		log.Println("Something went wrong: ", err)
-// 	})
-
-// 	c.OnResponse(func(r *colly.Response) {
-// 		fmt.Println("Page visited: ", r.Request.URL)
-// 	})
-
-// 	c.OnHTML(".k-p-4", func(e *colly.HTMLElement) {
-// 		fmt.Println("categoryLink: ", e.ChildAttr("div > a", "href"))
-// 		categoryLink := e.ChildAttr("div > a", "href")
-// 		getUnderCategories(categoryLink)
-// 	})
-
-// 	c.OnScraped(func(r *colly.Response) {
-// 		fmt.Println(r.Request.URL, " scraped!")
-// 	})
-
-// 	c.Visit("https://oda.com/no/products/")
-// }
-
-// func getUnderCategories(categoryLink string) {
-// 	c := colly.NewCollector()
-
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Visiting: ", r.URL)
-// 	})
-
-// 	c.OnError(func(_ *colly.Response, err error) {
-// 		log.Println("Something went wrong: ", err)
-// 	})
-
-// 	c.OnResponse(func(r *colly.Response) {
-// 		fmt.Println("Page visited: ", r.Request.URL)
-// 	})
-
-// 	c.OnHTML("section", func(e *colly.HTMLElement) {
-// 		fmt.Println("underCategoryLink: ", e.ChildAttr("section > a", "href"))
-// 		underCategoryLink := e.ChildAttr("section > a", "href")
-// 		getProductInfo(underCategoryLink)
-// 	})
-
-// 	c.OnScraped(func(r *colly.Response) {
-// 		fmt.Println(r.Request.URL, " scraped!")
-// 	})
-
-// 	link := fmt.Sprintf("https://oda.com%s", categoryLink)
-
-// 	c.Visit(link)
-// }
-
-// func getProductInfo(underCategoryLink string) {
-// 	c := colly.NewCollector()
-
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Visiting: ", r.URL)
-// 	})
-
-// 	c.OnError(func(_ *colly.Response, err error) {
-// 		log.Println("Something went wrong: ", err)
-// 	})
-
-// 	c.OnResponse(func(r *colly.Response) {
-// 		fmt.Println("Page visited: ", r.Request.URL)
-// 	})
-
-// 	c.OnHTML("article", func(e *colly.HTMLElement) {
-// 		fmt.Println("Title: ", e.ChildText("div > div h2"))
-// 		fmt.Println("Price: ", e.ChildText("div > div > div > span"))
-// 		fmt.Println("Kilopris: ", e.ChildText("div > div > div > p"))
-// 		fmt.Println("Bildelenke: ", e.ChildAttr("div > img", "src"))
-// 	})
-
-// 	c.OnScraped(func(r *colly.Response) {
-// 		fmt.Println(r.Request.URL, " scraped!")
-// 	})
-
-// 	n := colly.NewCollector()
-
-// 	n.OnHTML(".k-pill--extra-small", func(e *colly.HTMLElement) {
-// 		fmt.Println("Title: ", e.ChildText("main > div > div > div > span > div > div > span"))
-// 	})
-
-// 	link := fmt.Sprintf("https://oda.com%s&cursor=", underCategoryLink)
-
-// 	c.Visit(link)
-// }
-
-func getProductInfo() {
+func getCategories() {
 	c := colly.NewCollector()
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
+	c.OnHTML(".k-p-4", func(e *colly.HTMLElement) {
+		categoryLink := e.ChildAttr("div > a", "href")
+		getUnderCategories(categoryLink)
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong: ", err)
+	c.Visit("https://oda.com/no/products/")
+}
+
+func getUnderCategories(categoryLink string) {
+	c := colly.NewCollector()
+
+	fmt.Println("going to: ", categoryLink)
+
+	c.OnHTML("section", func(e *colly.HTMLElement) {
+		underCategoryLink := e.ChildAttr("section > a", "href")
+		getPageCount(underCategoryLink)
 	})
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Page visited: ", r.Request.URL)
-	})
+	link := fmt.Sprintf("https://oda.com%s", categoryLink)
 
-	c.OnHTML("article", func(e *colly.HTMLElement) {
-		fmt.Println("Title: ", e.ChildText("div > div h2"))
-		fmt.Println("Price: ", e.ChildText("div > div > div > span"))
-		fmt.Println("Kilopris: ", e.ChildText("div > div > div > p"))
-		fmt.Println("Bildelenke: ", e.ChildAttr("div > img", "src"))
-	})
+	c.Visit(link)
+}
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, " scraped!")
-	})
+func getPageCount(underCategoryLink string) {
+	c := colly.NewCollector()
 
-	n := colly.NewCollector()
-
-	n.OnHTML("main", func(e *colly.HTMLElement) {
-		fmt.Println("Antall produkter:  ", e.ChildText("main > div > div > div > span > div > div > a.k-choice-chip--selected.k-choice-chip--primary > span.k-pill--extra-small"))
+	c.OnHTML("main", func(e *colly.HTMLElement) {
 		tall := strings.Split(e.ChildText("main > div > div > div > span > div > div > a.k-choice-chip--selected.k-choice-chip--primary > span.k-pill--extra-small"), "")
 		tallLengde := len(tall) / 2
 		var s []string
@@ -144,14 +48,32 @@ func getProductInfo() {
 		str := strings.Join(s, "")
 		antallVarer, err := strconv.ParseFloat(str, 64)
 		if err != nil {
-			fmt.Println("Oh no")
+			return
 		}
-		underCategoryLink := "/no/categories/1135-bakeri-og-konditori/1136-handverksbakeri/?filters="
 		antallVarerDelt := antallVarer / 24
 		antallSider := int(math.Ceil(float64(antallVarerDelt)))
 		fmt.Println(antallSider)
-		link := fmt.Sprintf("https://oda.com%s&cursor=%v", underCategoryLink, antallSider)
-		c.Visit(link)
+		underCategoryLink := "/no/categories/1135-bakeri-og-konditori/1136-handverksbakeri/?filters="
+		link := fmt.Sprintf("https://oda.com%s", underCategoryLink)
+		for i := range antallSider {
+			getProductInfo(link, i+1)
+		}
 	})
-	n.Visit("https://oda.com/no/categories/1135-bakeri-og-konditori/1136-handverksbakeri/?filters=&cursor=2")
+	link := fmt.Sprintf("https://oda.com%s", underCategoryLink)
+	c.Visit(link)
+}
+
+func getProductInfo(link string, cursor int) {
+	c := colly.NewCollector()
+
+	c.OnHTML("article", func(e *colly.HTMLElement) {
+		fmt.Println("Title: ", e.ChildText("div > div h2"))
+		fmt.Println("Price: ", e.ChildText("div > div > div > span"))
+		fmt.Println("Kilopris: ", e.ChildText("div > div > div > p"))
+		fmt.Println("Bildelenke: ", e.ChildAttr("div > img", "src"))
+	})
+
+	visitLink := fmt.Sprintf("%s&cursor=%v", link, cursor)
+	fmt.Println("Link", visitLink)
+	c.Visit(visitLink)
 }
