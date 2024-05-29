d22d585977b890ca37a7bea607c751a9f04ef945
korneliushen <korneliussen06@gmail.com>
2024-05-29 17:15:19 +0200

    Bruker searchparams og prisma sin full text search feature for å søke

--- nettside/src/routes/+page.server.ts
+++ nettside/src/routes/+page.server.ts
@@ -1,7 +1,18 @@
 import prisma from "$lib/prisma";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ url }) => {
+    if (url.searchParams.get("search")) {
+        return {
+            products: await prisma.products.findMany({
+                where: {
+                  title: {
+                    search: url.searchParams.get("search") as string,
+                  },
+                },
+              })
+        }
+    }
     return {
         products: await prisma.products.findMany(
             {
@@ -9,5 +20,4 @@ export const load: PageServerLoad = async () => {
             }
         )
     }
-        
 };
