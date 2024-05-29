c63c7707e4cb5fd253cae98e65d891461d23c194
ErikAndreasKlokk <eaklokk@gmail.com>
2024-05-29 20:08:51 +0200

    fiksa bilder på produktcard

--- nettside/src/lib/components/productcard.svelte
+++ nettside/src/lib/components/productcard.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <div class=" z-20 w-44 md:w-60 h-full border border-borderColor/26 rounded-lg flex flex-col justify-between hover:border-purple-500 shadow-sm hover:shadow-md hover:scale-[1.01] transition p-2">
-    <a href="/produkt/{product.id}" title="product" class=" relative max-h-40 flex justify-center py-1 aspect-square">
-        <img loading="lazy" src={product.imagelink} alt="produktbilde">
+    <a href="/produkt/{product.id}" title="product" class=" relative max-h-40 lg:w-full flex justify-center items-center py-1 aspect-square">
+        <img class=" max-h-40" loading="lazy" src={product.imagelink} alt="produktbilde">
         <img loading="lazy" class=" absolute bottom-0 left-0 w-10 rounded-md ml-2" src="/{product.prices[0].Store}.svg" alt="nettside">
     </a>
     <div class=" pb-1">
