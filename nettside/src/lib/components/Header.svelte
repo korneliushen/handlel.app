3f1871151cf0138438472303ec6f7339112f8258
ErikAndreasKlokk <eaklokk@gmail.com>
2024-06-03 21:45:03 +0200

    la til en counter over handlelapp i header som viser hvor mange produkter du har i handlelappen din
    usikker på om vi også vil ha toasts som sier ifra når et produkt er lagt til i handlelappen

--- nettside/src/lib/components/Header.svelte
+++ nettside/src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { Search, User, Home } from 'lucide-svelte';
 	import { onMount } from 'svelte';
+	import { handlelapp } from '$lib/stores/handlelapp';
 
 	let screensize = 0;
 
@@ -36,7 +37,10 @@
 		<div class=" flex flex-1 items-center justify-end gap-4 font-medium">
 			<a href="/">Hjem</a>
 			<a href="/">Produkter</a>
-			<a href="/handlelapp">Handlelapp</a>
+			<a class=" relative" href="/handlelapp">
+				Handlelapp
+				<p class=" absolute top-[-17px] right-[-10px] border border-red-600 rounded-full text-sm w-6 h-6 place-items-center grid text-red-600 font-bold">{$handlelapp.length}</p>
+			</a>
 			<button
 				class=" flex h-10 w-10 items-center justify-center rounded-md bg-mainPurple text-white transition active:scale-95"
 				><User /></button
@@ -56,9 +60,10 @@
 				<Search size="25px" />
 				<p class=" mt-1 text-xs text-black">Søk</p>
 			</a>
-			<a class=" flex flex-col items-center" href="/handlelapp">
+			<a class=" flex flex-col items-center relative" href="/handlelapp">
 				<img class=" h-[25px]" src="/handlelapp.png" alt="" />
 				<p class=" mt-1 text-xs text-black">Handlelapp</p>
+				<p class=" absolute top-[-5px] right-[-5px] rounded-full text-sm w-5 h-5 place-items-center grid text-red-600 font-bold">{$handlelapp.length}</p>
 			</a>
 			<a class=" flex flex-col items-center" href="/">
 				<User size="25px" />
