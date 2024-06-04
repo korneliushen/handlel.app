fd8b024c5711f03462af51a1c8bb20343ef5c3a3
Elvinos33 <elvinos33@duck.com>
2024-06-04 09:44:04 +0200

    Bedre SEO

--- nettside/src/routes/+layout.svelte
+++ nettside/src/routes/+layout.svelte
@@ -3,10 +3,18 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<main class="flex absolute inset-0 font-sans items-center min-h-screen overflow-x-hidden overflow-y-scroll flex-col">
-  <Header/>
-  <div class=" mt-1 sm:mt-0">
-    <slot />
-  </div>
-
+<svelte:head>
+	<title>Handlelapp</title>
+	<meta
+		name="description"
+		content="Få full kontroll over matbudsjettet ditt med HandleLapp. Sammenlign priser fra alle de store norske kjedene, legg varer til handlelister og finn den billigste butikken for deg."
+	/>
+</svelte:head>
+<main
+	class="absolute inset-0 flex min-h-screen flex-col items-center overflow-x-hidden overflow-y-scroll font-sans"
+>
+	<Header />
+	<div class=" mt-1 sm:mt-0">
+		<slot />
+	</div>
 </main>
